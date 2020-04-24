51f1172fb766b8e33e94c8f1d2c99b740e0cf741
client-creator <juanhapes@gmail.com>
2020-04-24 11:55:24 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { ClientCreator } = require('./lib');
+
+module.exports = ClientCreator;
---
+++ tests/client-creator-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const assert = require('assert');
+
+const ClientCreator = require('./../index');
+
+const ClientCreatorError = require('./../lib/client-creator-error')
+
+describe('ClientCreator', () => {
+
+	// your tests here...
+
+});
